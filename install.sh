2ea973383e91fea37e97dd0465133da98fd7d95f
Tobias <50780566+LikeAnShadow@users.noreply.github.com>
2023-11-27 23:40:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Prüfe, welche Pakete installiert sind
 installed_packets=""
 echo "Prüfe, ob Pakete installiert sind.\n"
-! dpkg -s build-essential > /dev/null && installed_packets="build-essential " && echo "build-essential nicht installiert!"
+[! dpkg -s build-essential > /dev/null] && installed_packets="build-essential " && echo "build-essential nicht installiert!"
 [ "$( dpkg -l | grep mtools )"  = "" ] && installed_packets="${installed_packets}mtools " && echo "mtools nicht installiert!"
 [ "$( dpkg -l | grep grub-pc-bin )"  = "" ] && installed_packets="${installed_packets}grub-pc-bin " && echo "grub-pc-bin nicht installiert!"
 [ "$( dpkg -l | grep gdb )"  = "" ] && installed_packets="${installed_packets}gdb " && echo "gdb nicht installiert!"
