2582adafdf6986205d08a7cd3a899e1ffdb89f52
Tobias <50780566+LikeAnShadow@users.noreply.github.com>
2023-11-27 23:32:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 # Prüfe, welche Pakete installiert sind
 installed_packets=""
 echo "Prüfe, ob Pakete installiert sind.\n"
-[ "$( dpkg -l | grep build-essential )"  = "" ] && installed_packets="build-essential " && echo "build-essential nicht installiert!"
+! dpkg -s build-essential > /dev/null && installed_packets="build-essential " && echo "build-essential nicht installiert!"
 [ "$( dpkg -l | grep mtools )"  = "" ] && installed_packets="${installed_packets}mtools " && echo "mtools nicht installiert!"
-[ "$( dpkg -l | grep grub-pc-bin )"  = "" ] && installed_packet="${installed_packets}grub-pc-bin " && echo "grub-pc-bin nicht installiert!"
+[ "$( dpkg -l | grep grub-pc-bin )"  = "" ] && installed_packets="${installed_packets}grub-pc-bin " && echo "grub-pc-bin nicht installiert!"
 [ "$( dpkg -l | grep gdb )"  = "" ] && installed_packets="${installed_packets}gdb " && echo "gdb nicht installiert!"
 [ "$( dpkg -l | grep ddd )"  = "" ] && installed_packets="${installed_packets}ddd " && echo "ddd nicht installiert!"
 [ "$( dpkg -l | grep nasm )"  = "" ] && installed_packets="${installed_packets}nasm " && echo "nasm nicht installiert!"
