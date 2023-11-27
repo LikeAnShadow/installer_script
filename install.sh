25871f42e7f28fdc4673c88f2785d7d68eb29ada
Tobias <50780566+LikeAnShadow@users.noreply.github.com>
2023-11-28 00:23:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Prüfe, welche Pakete installiert sind
-WARN="\[\033[1;31;40m\u@\h:\w\$ \]"
-ALL_OK="\[\033[1;32;40m\u@\h:\w\$ \]"
-NORMAL="\[\033[0;37;40m\u@\h:\w\$ \]"
+WARN='\[\033[1;31;40m\u@\h:\w\$ \]'
+ALL_OK='\[\033[1;32;40m\u@\h:\w\$ \]'
+NORMAL='\[\033[0;37;40m\u@\h:\w\$ \]'
 installed_packets=""
-echo "Prüfe, ob Pakete installiert sind.\n"
-echo $WARN
+echo "Prüfe, ob Pakete installiert sind."
+echo ${WARN}
 [ "$( dpkg -l | grep build-essential )"  = "" ] && installed_packets="build-essential " && echo "build-essential nicht installiert!"
 [ "$( dpkg -l | grep mtools )"  = "" ] && installed_packets="${installed_packets}mtools " && echo "mtools nicht installiert!"
 [ "$( dpkg -l | grep grub-pc-bin )"  = "" ] && installed_packets="${installed_packets}grub-pc-bin " && echo "grub-pc-bin nicht installiert!"
@@ -15,12 +15,12 @@ echo $WARN
 [ "$( dpkg -l | grep qemu-system-x86 )"  = "" ] && installed_packets="${installed_packets}qemu-system-x86 " && echo "qemu-system-x86 nicht installiert!"
 [ "$( dpkg -l | grep xorriso )"  = "" ] && installed_packets="${installed_packets}xorriso " && echo "xorriso nicht installiert!"
 [ "$( dpkg -l | grep git )"  = "" ] && installed_packets="${installed_packets}git " && echo "git nicht installiert!"
-echo $NORMAL
-if $installed_packets != ""; then
+echo ${NORMAL}
+if "$installed_packets" != ""; then
   sudo apt update
   sudo apt -y install $installed_packets
 else
-  echo $ALL_OK "Alle Pakete bereits installiert!" $NORMAL
+  echo ${ALL_OK} "Alle Pakete bereits installiert!" ${NORMAL}
 fi
 git clone https://github.com/likeanshadow/oostubs_changed
 cd oostubs_changed
